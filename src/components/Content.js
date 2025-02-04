159f980bb8b198b4ee5d5a2fdc95143eec52451b
yuyu mansour <yomnamansour@Yomnas-MacBook-Air.local>
2025-02-04 16:52:35 -0500

    Initial commit with React static page components

---
+++ src/components/Content.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <main>
+      <p>The story Cinderella is a classic fairy tale about a meek young woman,
+         who, after her mother died, became the servant of her cruel stepmother and 
+stepsisters.
+         They make her do all the housework and sleep near the fireplace, from which she 
+takes the name Cinderella.
+         But even in dire circumstances, Cinderella was gentle and kind-hearted.</p>
+
+      <p>One day, the royal family put out word that there would be a grand ball so that the 
+prince could seek a bride.
+        Cinderellas stepmother and step-sisters went off to the ball, leaving Cinderella 
+behind.
+        But her fairy godmother appeared and magically gave Cinderella a fine dress, a pair 
+of glass slippers,
+        and a carriage but warned that the charm would wear off at midnight.
+        The prince fell in love with Cinderella and danced alone with her. But as midnight 
+approached, she fled,
+        leaving one slipper behind.</p>
+
+      <p>The prince goes around looking for the maiden and uses the slipper to find her.
+        When he comes to Cinderellas house, her stepsisters attempt unsuccessfully to fit the 
+slipper onto their feet.
+        Finally, Cinderella is brought in, and you will never guess. It fits her!
+        The prince recognizes her as his long-sought lady of the ball and marries beautiful 
+Cinderella,
+        who forgives her cruel stepmother and stepsisters and after this, they live happily 
+together. </p>
+    </main>
+  );
+};
+
+export default Content;
+
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Cinderella is about various lessons she teaches us that stand the test of time: 
+kindness is one.
+         Although she suffers ill treatment from her stepfamily, she remains kind and 
+forgiving in her heart and ultimately emerges happy.
+         In that, it is underscored that keeping ones integrity and acts of kindness even in 
+periods of misfortune shall
+         indeed lead to a good reward.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
